1538a8c3b2a29b2e255df3a692e9bd4d68ca1cbe
Romulo Rocha <romulorocha063@gmail.com>
2019-05-22 21:42:55 -0300

    cofigurações para início do projeto

--- android/app/src/main/java/com/marvel_react_native_redux_saga/MainActivity.java
+++ android/app/src/main/java/com/marvel_react_native_redux_saga/MainActivity.java
@@ -2,6 +2,11 @@ package com.marvel_react_native_redux_saga;
 
 import com.facebook.react.ReactActivity;
 
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+
 public class MainActivity extends ReactActivity {
 
     /**
@@ -11,5 +16,15 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "marvel_react_native_redux_saga";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+          return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
     }
 }
